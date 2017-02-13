edc833e32700d99a74889dd3f95007a656444e39
rkwon <rynkwn@gmail.com>
2017-02-13 17:01:21 -0500

    Playing around with Jsoup right now.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jars Used
+- jsoup-1.10.2 for HTML parsing.
--- src/browser/Browser.java
+++ src/browser/Browser.java
@@ -2,6 +2,21 @@
 // Consider https://limpet.net/mbrubeck/2014/08/08/toy-layout-engine-1.html
 // to be inspiration.
 
+// The Browser class will primarily be used to 
+
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+
 public class Browser {
+
     
+
+    public static void main(String[] args) {
+	try {
+	    Document doc = Jsoup.connect("https://jsoup.org/cookbook/input/load-document-from-url").get();
+	    System.out.println(doc);
+	} catch(Exception e) {
+	    System.err.println(e);
+	}
+    }
 }
