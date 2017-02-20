2d79ad060872c8b8c761ea9da4d7d8fa51ae3ffe
Ryan Kwon <rynkwn@gmail.com>
2017-02-19 23:56:49 -0500

    Moving methods around. Fleshing out the Browser class more. Ideally, want to be aware of grabable video files soon.

--- src/browser/Browser.java
+++ src/browser/Browser.java
@@ -9,6 +9,7 @@
  */
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
@@ -25,57 +26,71 @@ import java.awt.Toolkit;
 
 // Some code inspired by http://www.sourcecodester.com/tutorials/java/5732/simple-web-browser-java.html
 public class Browser extends JPanel {
+	String url;
+	Document doc;
+
     JEditorPane content;
 	JScrollPane scrollPane;
 
-    public Browser() {
-	setLayout(new BorderLayout(1, 1));
-	
-	content = new JEditorPane();
-	content.setContentType("text/html");
-	content.setEditable(false);
-	
-	scrollPane = new JScrollPane(content);
-	
-	add(scrollPane, BorderLayout.CENTER);
+    public Browser(String URL) {
+		this.url = URL;
+		doc = Jsoup.connect(url).get();
+		
+		setLayout(new BorderLayout(1, 1));
+		
+		content = new JEditorPane();
+		content.setContentType("text/html");
+		content.setEditable(false);
+		
+		scrollPane = new JScrollPane(content);
+		
+		add(scrollPane, BorderLayout.CENTER);
     }
 
     // Bandaid method for now.
     public void updateContent(String htmlData) {
-	try {
-	    //content.setPage(htmlData);
-		content.setPage(htmlData);
-	} catch(IOException e) {
-		e.printStackTrace();
-	}
+		try {
+			//content.setPage(htmlData);
+			content.setPage(htmlData);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
     }
+	
+	public ArrayList<String> listVideoFiles() {
+		return new ArrayList<String>();
+	}
 
-    // 
+    // Main method.
     public static void main(String[] args) {
 
-	JFrame window = new JFrame("Browser");
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	
-	int width = (int) (screenSize.getWidth() * .75);
-	int height = (int) (screenSize.getHeight() * .75);
-	
-	window.setSize(width, height);
-	
-	Browser browser = new Browser();
-	window.add(browser);
-	
-	
-	try {
-	    String url = "https://www.youtube.com/watch?v=9bZkp7q19f0";
-	    //Document doc = Jsoup.connect(url).get();
-	    //System.out.println(doc.toString());
-	    browser.updateContent(url);
+		JFrame window = new JFrame("Browser");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		int width = (int) (screenSize.getWidth() * .75);
+		int height = (int) (screenSize.getHeight() * .75);
+		
+		window.setSize(width, height);
+		
+		Browser browser;
+		
+		try {
+			String url = "https://www.youtube.com/watch?v=9bZkp7q19f0";
+			
+			
+			
+			
+			browser = new Browser(url);
+			browser.updateContent(url);
 
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}
+			window.add(browser);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 
-	window.setVisible(true);
+		window.setVisible(true);
     }
 }
