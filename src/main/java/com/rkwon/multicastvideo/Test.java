9a32d2b96baeccefee5c027016c2ec0e3abff08a
Ryan Kwon <rynkwn@gmail.com>
2017-04-02 22:45:22 -0400

    Finding the VLC libraries now. Will now progress with rest of tutorial.

--- src/main/java/com/rkwon/multicastvideo/Test.java
+++ src/main/java/com/rkwon/multicastvideo/Test.java
@@ -1,16 +1,25 @@
+import java.util.HashSet;
+import java.util.Arrays;
+
 import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+import com.sun.jna.NativeLibrary;
 
 public class Test {
+
 	public static void main(String[] args) {
 
 		System.out.println("Test started!");
+        
+		NativeDiscovery nativeDiscovery = new NativeDiscovery();
 
-		boolean found = new NativeDiscovery().discover();
-		System.out.println(found);
+		boolean found = false;
+
+		found = nativeDiscovery.discover();
 		
-		/*
+		System.out.println(found);
 		System.out.println(LibVlc.INSTANCE.libvlc_get_version());
-		*/
 	}
 }
