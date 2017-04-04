fa8577baf8dade43e1fe3a974f45f2e84e4124eb
Ryan Kwon <rynkwn@gmail.com>
2017-04-03 20:57:02 -0400

    HOLY POOP IT'S WORKING.

--- src/main/java/com/rkwon/multicastvideo/Utils.java
+++ src/main/java/com/rkwon/multicastvideo/Utils.java
@@ -4,16 +4,16 @@
 
 public class Utils {
 
-
+// :sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:duplicate{dst=rtp{dst=230.0.0.1,port=5555,mux=ts,sap,name=MyStream},dst=display} :sout-keep
 	public static String formatRtpStream(String serverAddress, int serverPort) {
-        StringBuilder sb = new StringBuilder(60);
-        
-        sb.append(":sout=#rtp{dst=");
-        sb.append(serverAddress);
-        sb.append(",port=");
-        sb.append(serverPort);
-        sb.append(",mux=ts}");
-        
-        return sb.toString();
+        StringBuilder sb = new StringBuilder(400);
+
+	    sb.append(":sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:rtp{dst=");
+
+	    sb.append(serverAddress);
+	    sb.append(",port=");
+	    sb.append(serverPort);
+	    sb.append(",mux=ts,sap}");
+	    return sb.toString();
     }
 }
