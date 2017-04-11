020093dec97d4dd59c7a9407bbbb97b0d6102d02
Ryan Kwon <rynkwn@gmail.com>
2017-04-10 23:45:30 -0400

    Fleshing out Network log and sync related infrastructure.

--- src/main/java/com/rkwon/multicastvideo/HostSync.java
+++ src/main/java/com/rkwon/multicastvideo/HostSync.java
@@ -5,7 +5,9 @@ import java.util.*;
 // A class to track and measure latency among clients in a session.
 // Will also be used to send relevant data to clients.
 
-public class HostSync extends Thread {
+public class HostSync implements Runnable {
+
+	public boolean keepRunning = true;
 
 	public int portNumber;
 	public ServerSocket serverSocket;
@@ -14,6 +16,7 @@ public class HostSync extends Thread {
 
 	public HostSync() {
 		portNumber = Constants.Network.HOST_SYNC_PORT;
+		networkLog = new NetworkLog();
 		
 		try {
 
@@ -31,6 +34,30 @@ public class HostSync extends Thread {
 	// Want to continue accepting connections. 
 	// For every accepted connection, grab relevant network data.
 	// Store relevant network data if appropriate option is selected
+
+	@Override
+	public void run() {
+		// Continue accepting connections
+		// For every accepted connection grab relevant network data.
+
+		while(keepRunning) {
+
+			try {
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			} catch(IOException e) {
+				networkLog.registerError();
+			}
+
+
+
+		}
+	}
+
+	public void stop() {
+		keepRunning = false;
+	}
 
 	// Log:
 	// Permanently save stored network data.
--- src/main/java/com/rkwon/multicastvideo/NetworkLog.java
+++ src/main/java/com/rkwon/multicastvideo/NetworkLog.java
@@ -8,23 +8,43 @@ import java.io.*;
 
 public class NetworkLog {
 
-	// Data points about date/time of this session.
-	public LocalDateTime = new LocalDateTime.now();
+	public static final int ERROR_ID = 500;
 
-	public int identifier;
+	// Data points about date/time of this session.
+	public LocalDateTime sessionStart = LocalDateTime.now();
 	public HashMap<Integer, ArrayList<NetworkDatum>> data;
 
 	public NetworkLog() {
 		data = new HashMap<Integer, ArrayList<NetworkDatum>>();
-		
+	}
+
+	// Register an error.
+	public void registerError() {
+		addToLog(ERROR_ID, new NetworkDatum(false, ERROR_ID));
+	}
+
+	// A helper method to add a datapoint to our log.
+	private void addToLog(int id, NetworkDatum dataPoint) {
+		if(data.containsKey(id)) {
+			data.get(id).add(dataPoint);
+		} else {
+			ArrayList<NetworkDatum> datum = new ArrayList<NetworkDatum>();
+			datum.add(dataPoint);
+			data.put(id, datum);
+		}
 	}
 
 
 	// A wrapper class around a single point of interesting/relevant network data.
 	class NetworkDatum {
 
-		public NetworkDatum() {
+		public boolean valid;
+		public LocalDateTime messageReceived = LocalDateTime.now();
+		public int identifier;
 
+		public NetworkDatum(boolean isValid, int id) {
+			valid = isValid;
+			identifier = id;
 		}
 
 	}
