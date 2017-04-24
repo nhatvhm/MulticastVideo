90b7ef22513ea67b43518873a1e8b3cab93997fb
rkwon <rynkwn@gmail.com>
2017-04-24 19:43:55 -0400

    Created the constructor for NetworkDatum.

--- src/main/java/com/rkwon/multicastvideo/NetworkDatum.java
+++ src/main/java/com/rkwon/multicastvideo/NetworkDatum.java
@@ -3,28 +3,67 @@ import java.awt.Dimension;
 // A wrapper class around a single point of interesting/relevant network data.
 // Intended to be exchanged between clients and the host for data aggregation.
 public class NetworkDatum {
+    
+    public String username; // username of the sender
+    
+    // Data components.
+    public String aspectRatio;
+    public int audioChannel;
+    public long audioDelay;
+    
+    public float fps;
+    public float mediaLength;
+    
+    // Position: what percentage of the video you're through
+    public float position;
+    
+    // Time: Current play-back time.
+    public long time;
+    
+    // 1.0 is normal speed, .5 is half speed, 2.0 is double speed.
+    public float videoPlayRate; 
+    public float scale;
+    
+    Dimension videoDimension;
 
-	public String username; // username of the sender
+    public NetworkDatum(String username,
+
+			String aspectRatio,
+			int audioChannel,
+			long audioDelay,
+
+			float fps,
+			float mediaLength,
+			
+			float position,
+			
+			long time,
+
+			float videoPlayRate,
+			float scale,
+			
+			Dimension videoDimension			
+			) {
+
+	this.username = username;
 	
-	// Data components.
-	public String aspectRation;
-	public int audioChannel;
-	public long audioDelay;
+	this.aspectRatio = aspectRatio;
+	this.audioChannel = audioChannel;
+	this.audioDelay = audioDelay;
+	
+	this.fps = fps;
+	this.mediaLength = mediaLength;
+	
+	this.position = position;
 
-	public float fps;
-	public float mediaLength;
+	this.time = time;
 
-	// Position: what percentage of the video you're through
-	public float position;
+	this.videoPlayRate = videoPlayRate;
+	this.scale = scale;
 
-	// Time: Current play-back time.
-	public long time;
-
-	// 1.0 is normal speed, .5 is half speed, 2.0 is double speed.
-	public float videoPlayRate; 
-	public float scale;
-
-	Dimension videoDimension;
-
-
+	this.videoDimension = videoDimension;
+	
+    }
+    
+    
 }
