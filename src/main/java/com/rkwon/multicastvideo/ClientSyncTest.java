80873dc136fbfb596c8bc78bc2d876ac3140d9f2
rkwon <rynkwn@gmail.com>
2017-04-24 20:59:19 -0400

    Some minor changes made to the sync tests.

--- src/main/java/com/rkwon/multicastvideo/ClientSyncTest.java
+++ src/main/java/com/rkwon/multicastvideo/ClientSyncTest.java
@@ -1,7 +1,9 @@
 public class ClientSyncTest {
 	public static void main(String [] args) {
 		System.out.println("Starting Client Sync Test");
-		ClientSync client = new ClientSync(null, "137.165.8.126", Constants.Network.HOST_SYNC_PORT);
+		System.out.println("Username is: " + System.getProperty("user.name"));
+
+		ClientSync client = new ClientSync(null, "137.165.8.120", Constants.Network.HOST_SYNC_PORT);
 		Thread clientThread = new Thread(client);
 
 		clientThread.start();
