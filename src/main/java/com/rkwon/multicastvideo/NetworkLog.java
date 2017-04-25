6ea8165e025bc0d7828cc078df974c665c81a38b
Ryan Kwon <rynkwn@gmail.com>
2017-04-25 14:17:47 -0400

    Made a deprecated folder to hold some no-longer used files.

--- src/main/java/com/rkwon/multicastvideo/NetworkLog.java
+++ src/main/java/com/rkwon/multicastvideo/NetworkLog.java
@@ -10,13 +10,7 @@ import java.io.*;
 // A class that logs important relevant information for our analyses.
 
 public class NetworkLog {
-
-	// Types
-	public static final String ERROR = "ERROR";
-	public static final String LATENCY_MEASUREMENT = "LATENCY";
-	public static final String VIDEO_POSITION = "VIDEO_POSITION";
-	public static final String CLIENT_JOIN = "CLIENT_JOIN";
-
+	
 	// Data points about date/time of this session.
 	//public LocalDateTime sessionStart = LocalDateTime.now();
 	public Date sessionStart = new Date();
